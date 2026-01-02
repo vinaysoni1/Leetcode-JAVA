509f70bb74e72a4e57e6a944370b1116bb91b618
vinaysoni1 <vinaysoni9174@gmail.com>
2026-01-02 15:58:22 +0530

    Roman to Integer convert

---
+++ DAY8/solution.java
@@ -0,0 +1,39 @@
+ // roman to integer
+
+import java.util.Map;
+
+class solution {
+    static int romanToInt(String s){
+Map<Character, Integer> romanMap = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+        );
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char currentChar = s.charAt(i);
+            int currentValue = romanMap.get(currentChar);
+
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return total;
+    
+}
+public static void main(String[] args) {
+        System.out.println(romanToInt("IX")); // Example usage
+    }
+}
