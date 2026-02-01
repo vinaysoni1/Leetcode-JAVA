a61b79a73427f787d19a079257d59f3606559d49
vinaysoni1 <vinaysoni9174@gmail.com>
2026-02-01 21:17:24 +0530

    divide two integer

--- DAY16/dividetwointeger.java
+++ DAY16/dividetwointeger.java
@@ -1,3 +1,51 @@
-public class dividetwointeger {
-    
+class dividetwointeger {
+  static int divide(int dividend, int divisor) {
+        
+        if (divisor == 1) {
+            return dividend;
+        }
+      
+        
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+      
+        
+        boolean isPositiveResult = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0);
+      
+       
+        dividend = dividend > 0 ? -dividend : dividend;
+        divisor = divisor > 0 ? -divisor : divisor;
+      
+        int quotient = 0;
+      
+        
+        while (dividend <= divisor) {
+           
+            int currentDivisor = divisor;
+            int currentQuotient = 1;
+          
+           
+            while (currentDivisor >= (Integer.MIN_VALUE >> 1) && dividend <= (currentDivisor << 1)) {
+                currentDivisor <<= 1; 
+                currentQuotient <<= 1;  
+            }
+          
+           
+            quotient += currentQuotient;
+          
+        
+            dividend -= currentDivisor;
+        }
+      
+      
+        return isPositiveResult ? quotient : -quotient;
+    }
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        int result = divide(dividend, divisor);
+        System.out.println("Quotient: " + result);
 }
+}
+    
