71366f82a24a5fc90aec41742b95c7a0a724f6d9
vinaysoni1 <vinaysoni9174@gmail.com>
2026-02-02 22:22:33 +0530

    merge intarval

---
+++ DAY17/MERGE_INTERVAL.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.*;
+class MERGE_INTERVAL {
+
+    static ArrayList<int[]> mergeOverlap(int[][] arr) {
+        int n = arr.length;
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        ArrayList<int[]> res = new ArrayList<>();
+
+        // Checking for all possible overlaps
+        for (int i = 0; i < n; i++) {
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            // Skipping already merged intervals
+            if (!res.isEmpty() && res.get(res.size() - 1)[1] >= end) {
+                continue;
+            }
+
+            // Find the end of the merged range
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                }
+            }
+            res.add(new int[]{start, end});
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{7, 8}, {1, 5}, {2, 4}, {4, 6}};
+        ArrayList<int[]> res = mergeOverlap(arr);
+
+        for (int[] interval : res) {
+            System.out.println(interval[0] + " " + interval[1]);
+        }
+    }
+}
