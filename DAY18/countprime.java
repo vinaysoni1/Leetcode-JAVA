cd3513b23dfdae760dee0b64be8636fc94cae798
vinaysoni1 <vinaysoni9174@gmail.com>
2026-02-03 21:34:40 +0530

    count prime number

---
+++ DAY18/countprime.java
@@ -0,0 +1,23 @@
+class countprime{
+    static int Prime(int n){
+        int count = 0;
+        for(int i=2;i<=n;i++){
+            boolean isPrime = true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                count++;
+            }
+        }
+        return count; 
+    }
+    public static void main(String [] args){
+        int n = 10;
+        System.out.println(Prime(n));
+
+    }
+}
